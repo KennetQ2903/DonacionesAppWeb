820a5d0e727ef91c4e0115d7894978cf60e360c2
KennetQ2903 <78316880+KennetQ2903@users.noreply.github.com>
2021-01-31 12:56:16 -0600

    Primera version web, contiene el area de donaciones y layout de prueba para el area

---
+++ Donacionesv5/Donacionesv5/Areas/Donaciones/Controllers/DonacionesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Donacionesv5.Areas.Donaciones.Controllers
+{
+    [Area("Donaciones")]
+    public class DonacionesController : Controller
+    {
+        public IActionResult Donacion()
+        {
+            return View();
+        }
+    }
+}
